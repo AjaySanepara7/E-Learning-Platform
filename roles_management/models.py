716304a25e28fb3c172c04e04742cf6ac2a55967
AjaySanepara7 <ajaysanepara7@gmail.com>
2024-12-17 19:23:31 +0530

    feat: define profile model in the models.py file

--- roles_management/models.py
+++ roles_management/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from phone_field import PhoneField
+from django_countries.fields import CountryField
+from django_extensions.db.models import TimeStampedModel
 
-# Create your models here.
+
+class Profile(TimeStampedModel):
+    person_gender = [
+        ("M", "Male"),
+        ("F", "Female"),
+        ("O", "Others")
+    ]
+
+    countries = [
+        ("I", "India")
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    gender = models.CharField(max_length=100, choices=person_gender)
+    date_of_birth = models.DateField()
+    mobile = PhoneField(blank=True, help_text='Contact phone number', unique=True)
+    country = CountryField()
+    profie_picture = models.ImageField(upload_to='images/', null=True, blank=True)
+    resume = models.FileField(upload_to='resume/', null=True, blank=True)
+
+
+
+
