d8187fae4f89e3ecb506698e3ff89f5dc1d20b48
AjaySanepara7 <ajaysanepara7@gmail.com>
2024-12-30 09:38:58 +0530

    Feat: Added Reset password functionality

--- roles_management/urls.py
+++ roles_management/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("signup_page", views.SignupPage.as_view(), name="signup_page"),    
     path("dashboard", login_required(views.Dashboard.as_view()), name="dashboard"),    
     path("enroll", login_required(views.Enroll.as_view()), name="enroll"),
+    path("reset_password", views.ResetPassword.as_view(), name="reset_password"),  
 ]
