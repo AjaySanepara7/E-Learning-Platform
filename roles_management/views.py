fdf7f8d2158c1243ee00ea4286d1dc16b730d8bf
AjaySanepara7 <ajaysanepara7@gmail.com>
2025-01-02 13:38:48 +0530

    fix: updated roles_management app's views.py file

--- roles_management/views.py
+++ roles_management/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.views import View
 from django.conf import settings
-from roles_management.models import Enrollment, Course
 from roles_management.forms import UserForm, ProfileForm, EnrollmentForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import Permission
